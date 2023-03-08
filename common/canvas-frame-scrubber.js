250b4c8f4613a3edfa37f2eacb5075db8a7321a5
Austin Olijar <Austin.Olijar@gmail.com>
2023-03-08 11:21:27 -0600

    Add files via upload

--- common/canvas-frame-scrubber.js
+++ common/canvas-frame-scrubber.js
@@ -1,23 +1,27 @@
 const CanvasFrameScrubber = (() => {
-    const create = (context, frames) => {
-        let currentFrame = 0;
+  const create = (context, frames) => {
+    let currentFrame = 0;
 
-        const observer = {
-            next: percentage => {
-                const frameIndex = Math.floor((percentage * (frames.length - 1)) / 100);
+    const observer = {
+      next: (percentage) => {
+        const frameIndex = Math.floor((percentage * (frames.length - 1)) / 100);
 
-                if (currentFrame === frameIndex) return;
+        if (currentFrame === frameIndex) return;
 
-                window.requestAnimationFrame(() => {
-                    context.drawImage(frames[frameIndex], 0, 0);
-                });
-            }
-        };
-
-        return observer;
+        window.requestAnimationFrame(() => {
+          // THIS IS ADDED TO SCRUB OLD PNG
+          const canvas = document.querySelector(".canvas");
+          const context = canvas.getContext("2d");
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          context.drawImage(frames[frameIndex], 0, 0);
+        });
+      },
     };
 
-    return {
-        create: create
-    };
+    return observer;
+  };
+
+  return {
+    create: create,
+  };
 })();
